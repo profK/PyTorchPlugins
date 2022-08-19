402a7fee66875992841191c381eb3850314fae3c
Jeff Kesselman <jpkessel@purdue.edu>
2022-08-19 08:29:24 -0400

    Includign torch.h was hanging DLL, removed from PyTorchDLL

--- PyTorchDLL/Source/PyTorchDLL/PyTorchDLL.Build.cs
+++ PyTorchDLL/Source/PyTorchDLL/PyTorchDLL.Build.cs
@@ -1,18 +1,21 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class PyTorchDLL : ModuleRules
 {
 	public PyTorchDLL(ReadOnlyTargetRules Target) : base(Target)
 	{
+		bUseRTTI = true;
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
 		PublicIncludePaths.AddRange(
 			new string[] {
-				// ... add public include paths required here ...
+				Path.Combine(ModuleDirectory, 
+					"Public/torch/csrc/api/include")
 			}
-			);
+		);
 				
 		
 		PrivateIncludePaths.AddRange(
@@ -20,7 +23,19 @@ public class PyTorchDLL : ModuleRules
 				// ... add other private include paths required here ...
 			}
 			);
-			
+		
+		//For linking at build
+		DirectoryInfo piTorchLibDir = 
+			new DirectoryInfo(Path.Combine(ModuleDirectory,
+				"../../Binaries/ThirdParty/PyTorchDLLLibrary/win64"));
+		foreach (FileInfo fInfo in piTorchLibDir.GetFiles("*.lib")){
+			PublicAdditionalLibraries.Add(fInfo.FullName);
+		}
+		
+		foreach (FileInfo fInfo in piTorchLibDir.GetFiles("*.dll")){
+			RuntimeDependencies.Add("$(BinaryOutputDir)/"+fInfo.Name,
+				fInfo.FullName);
+		}
 		
 		PublicDependencyModuleNames.AddRange(
 			new string[]
